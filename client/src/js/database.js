e3eb754cafb571bcb420d2e240d23e67a02af3e8
Michael Mount <mount.photos@gmail.com>
2023-04-10 14:42:23 -0600

    prepared for heroku dev

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -31,20 +31,6 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log("GET all from Database");
-
-  const jateDb = await openDB("jate", 1);
-
-  const tx = jateDb.transaction("jate", "readonly");
-
-  const store = tx.objectStore("jate");
-
-  const request = store.getAll();
-
-  const result = await request;
-
-  console.log("result.vaule", result);
-
-  return result.value;
 };
 
 initdb();
